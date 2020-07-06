4e78b3c4177be91de05060aae7e6ddcc480cd9be
Shunro Dozono <dozono@cba-japan.com>
2020-07-06 15:50:38 +0900

    display errors on Heroku

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
     echo "<h1>hello World!</h1><br/>";
     echo "Thank you for your time!<br/><br/>";
     echo "Development Group.<br/>";
